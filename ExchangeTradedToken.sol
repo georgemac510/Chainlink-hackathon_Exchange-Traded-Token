8f0ecc382bbf23e238aae736fedf9bed082536e8
georgemac510 <georgemac510@gmail.com>
2021-03-27 11:13:22 -0400

    Deployed ExchangeTradedToken.sol to Kovan

--- ExchangeTradedToken.sol
+++ ExchangeTradedToken.sol
@@ -1,14 +1,18 @@
-pragma solidity 0.6.7;
+pragma solidity  >=0.6.0 <0.9.0;
 //SPDX-License-Identifier: MIT
 
 //import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol"; //https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
 
-contract YourContract is ERC20{
+contract ExchangeTradedToken is ERC20{
 
-  constructor() public ERC20("ExchangeTradedToken", "EXTT") {
-    // what should we do on deploy?
-    _mint(msg.sender,10 ether);
-  }
+    constructor() public ERC20("ExchangeTradedToken", "EXTT") {
+        // what should we do on deploy?
+        _mint(msg.sender,10 ether);
+    }
 
 }
+
+
+
+//Kovan contract address: 0xdfa13fb46c44148211d9a9bbb36919d5093d90a0
