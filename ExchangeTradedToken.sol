dce37ea817142039d835393e0a3ce5e41f10baea
georgemac510 <georgemac510@gmail.com>
2021-03-30 11:27:41 -0400

    Add to contract

--- ExchangeTradedToken.sol
+++ ExchangeTradedToken.sol
@@ -8,7 +8,7 @@ contract ExchangeTradedToken is ERC20{
 
     constructor() public ERC20("ExchangeTradedToken", "EXTT") {
         // what should we do on deploy?
-        _mint(msg.sender,10 ether);
+        _mint(msg.sender,1000000 * (10 ** uint256(decimals())));
     }
 
 }
@@ -17,3 +17,6 @@ contract ExchangeTradedToken is ERC20{
 
 //Kovan contract address: 0xdfa13fb46c44148211d9a9bbb36919d5093d90a0
 //deployed 3-27-21 11:11 AM
+
+//new Kovan contract address: 0xd4472baba20e582211970eed601e40329e5337b6
+//deployed 3-29-21 11:53 AM
